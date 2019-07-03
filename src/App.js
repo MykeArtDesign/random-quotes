4d538a258b074cf365b46c32919396e2013bf3ec
MykeArt <mickael.sorhaindo@gmail.com>
2019-07-03 16:58:13 +0200

    v1.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import citations from './citations'
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App" id='quote-box'>
-      <text
-    </div>
-  );
+class App extends Component {
+   
+  state = {
+    text: 'Ma première citation pourrait bien être la dernière',
+    author: 'Myke'   
+  }
+
+  quoteClick = () => {
+    const quotesArray = Object.keys(citations)
+
+    const randomQuotes = quotesArray[Math.floor( Math.random() * quotesArray.length )]
+    if(this.state.text !== citations[randomQuotes].text){
+      this.setState(citations[randomQuotes])
+    } else {
+      this.quoteClick()
+    }
+  }
+
+  render(){
+    return (
+      <div id="quote-box" className="container-fluid">
+        <blockquote className="row card">
+        <q id="text" className="text-center card-text">{this.state.text}</q>
+        <cite id="author" className="text-center"> - {this.state.author} -</cite>
+        </blockquote>
+        <div className="d-flex flex-column align-items-center">
+          <button className="btn btn-primary" id="new-quote" onClick={() => {this.quoteClick()}}>Nouvelle citation</button>
+          <br/>
+          <a href="twitter.com/intent/tweet" id="tweet-quote">Partager la citation</a>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/citations.js
@@ -0,0 +1,51 @@
+module.exports = {
+    citation1: {
+        text: 'Mourir, ce n\'est rien. Commence donc par vivre. C\'est moins drôle et c\'est plus long',
+        author: 'Jean Anouilh'
+    },
+
+    citation2: {
+        text: 'Les raisons d\'aimer et de vivre varient comme font les saisons.',
+        author: 'Louis Aragon'
+    },
+
+    citation3: {
+        text: 'Le temps d\'apprendre à vivre, il est déjà trop tard.',
+        author: 'Louis Aragon'
+    },
+
+    citation4: {
+        text: 'La vie n\'est jamais aussi compliquée que se plaisent à l\'imaginer les têtes faibles.',
+        author: 'Marcel Aymé'
+    },
+
+    citation5: {
+        text: 'La vie est pleine de désillusions, de malheurs et de souffrances, et le pire c\'est qu\'elle est beaucoup trop courte.',
+        author: 'Woody Allen'
+    },
+
+    citation6: {
+        text: 'Si je recommençais ma vie je tâcherais de faire mes rêves encore plus grands ; parce que la vie est infiniment plus belle et plus grande que je n\'avais cru, même en rêve.',
+        author: 'Georges Bernanos'
+    },
+
+    citation7: {
+        text: 'L\'homme devrait mettre autant d\'ardeur à simplifier sa vie qu\'il en met à la compliquer.',
+        author: 'Henri Bergson'
+    },
+
+    citation8: {
+        text: 'Personne ne peut savoir si le monde est fantastique ou réel, et non plus s\'il existe une différence entre rêver et vivre.',
+        author: 'Jorge Luis Borges'
+    },
+
+    citation9: {
+        text: 'Vivre, c\'est ne pas se résigner.',
+        author: 'Albert Camus'
+    },
+
+    citation10: {
+        text: 'Heureuse ou malheureuse, la vie est le seul trésor que l\'homme possède, et ceux qui ne l\'aiment pas n\'en sont pas dignes.',
+        author: 'Giacomo Casanova'
+    }
+}
